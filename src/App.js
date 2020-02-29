58626dd40a7bb5c565a0ae926747ac19a18312bf
kupuma-ru21 <kupumaru21@gmail.com>
2020-02-29 23:40:48 +0900

    search queryとVariablesをアプリケーションに適用する

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { ApolloProvider } from 'react-apollo';
 import { Query } from 'react-apollo';
 
 import client from './client';
 import { ME } from './grahpql';
+import { SEARCH_REPOSITORIES } from './grahpql';
 
-const App = () => {
-  return (
-    <ApolloProvider client={client}>
-      <div>Hello Graphql</div>;
-      <Query query={ME}>
-        {({ loading, error, data }) => {
-          if (loading) return 'loading...';
-          if (error) return `Error! ${error.message}`;
-          return <div>{data.user.avatarUrl}</div>;
-        }}
-      </Query>
-    </ApolloProvider>
-  );
+const VARIABLES = {
+  first: 5,
+  after: null,
+  last: null,
+  before: null,
+  query: 'フロントエンドエンジニア',
 };
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = VARIABLES;
+  }
+  render() {
+    const { query, first, last, before, after } = this.state;
+    return (
+      <ApolloProvider client={client}>
+        <Query
+          query={SEARCH_REPOSITORIES}
+          variables={{ query, first, last, before, after }}
+        >
+          {({ loading, error, data }) => {
+            if (loading) return 'loading...';
+            if (error) return `Error! ${error.message}`;
+            console.log(data);
+            return <div></div>;
+          }}
+        </Query>
+      </ApolloProvider>
+    );
+  }
+}
 
 export default App;
